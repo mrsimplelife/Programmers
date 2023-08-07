c3e5fc0ff86ade4012626d57aa09d4cb6cbe56f7
youme <mrsimplelife@naver.com>
2023-08-07 11:38:15 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.09 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.js
@@ -0,0 +1,11 @@
+function solution(phone_number) {
+    let answer = '';
+    for(let i = 0; i<phone_number.length;i++){
+        if(phone_number.length - 4 > i){
+            answer += '*';
+        }else{
+            answer += phone_number[i];
+        }
+    }
+    return answer;
+}
